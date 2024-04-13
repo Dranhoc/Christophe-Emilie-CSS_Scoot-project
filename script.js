591dc8f825e96708364105639ed80268175fef69
Dranhoc <christophe.h.leroy@gmail.com>
2024-04-13 15:59:45 +0200

    Smooth navBar

--- script.js
+++ script.js
@@ -3,18 +3,20 @@
 const navbar = document.getElementById('myNav');
 
 // Initialisez une variable pour stocker la position de défilement précédente
-let prevScrollPos = window.pageYOffset;
+let prevScrollPos = window.scrollY;
 
 window.addEventListener('scroll', () => {
     // Obtenez la position de défilement actuelle
-    const currentScrollPos = window.pageYOffset;
+    const currentScrollPos = window.scrollY;
 
     // Si l'utilisateur défile vers le haut, ajoutez la classe 'sticky'
     if (prevScrollPos > currentScrollPos) {
         navbar.classList.add('sticky');
+        navbar.classList.remove('hidden');
     } else {
         // Sinon, retirez la classe 'sticky'
         navbar.classList.remove('sticky');
+        navbar.classList.add('hidden');
     }
 
     // Mettez à jour la position de défilement précédente
